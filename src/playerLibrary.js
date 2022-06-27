7a4557ca74c3c4055c65c215e908ba2fd9210fff
Harshada Chaudhari <charsh18@gmail.com>
2022-06-27 21:43:59 +0530

    added native player with hls stream and switch cases

--- src/playerLibrary.js
+++ src/playerLibrary.js
@@ -3,16 +3,35 @@ import { urls } from "./urls";
 
 export const init = (playerType, streamType, sDRMType, tokenType) => {
     console.log("Initiating player", playerType, streamType, sDRMType, tokenType)
-    let hlsPlayer = new Hls(Object.assign({})); 
+    let hlsPlayer = new Hls(Object.assign({}));
     let player = document.getElementById("video")
-    if (playerType === "hls" && streamType === "hls") {
-        if (sDRMType === "none") {
-            hlsPlayer.loadSource(urls.hls);
-            hlsPlayer.attachMedia(player);
-            hlsPlayer.startLoad();
-            player.play()
-        } else if (sDRMType === "aes") {
+
+    switch (playerType) {
+        case "hls":
+            if (streamType === "hls" && sDRMType === "none") {
+                hlsPlayer.loadSource(urls.hls1);
+                hlsPlayer.attachMedia(player);
+                hlsPlayer.startLoad();
+                player.play()
+            } else if (sDRMType === "aes") {
+                //
+            }
+            break;
+
+        case "native":
+            if (streamType === "hls") {
+                let player = document.getElementById("video")
+                player.src = urls.hls2
+                player.play()
+            }
+            break;
+
+        default:
             //
-        }
+            break;
     }
+
+
+
+
 }
--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,4 @@
 export const urls = {
-    "hls" : "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8"
+    "hls1" : "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+    "hls2" : "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
 }
