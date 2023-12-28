465f3daf2f03e3adf45d4945bc409948b118f6f3
Harshada Chaudhari <charsh18@gmail.com>
2023-12-28 20:20:16 +0100

    Play HLS on native player case added

--- src/players/nativePlayer.js
+++ src/players/nativePlayer.js
@@ -3,12 +3,12 @@ import { urls } from "../common/urls";
 export const nativePlayback = (player, streamType) => {
 
     switch (streamType) {
-
         case "hls":
-            let hlsUrl = document.getElementById("urls").value ?
-                document.getElementById("urls").value : urls.hls2
-            player.src = hlsUrl
-            player.play()
+            console.log("Playing native hls")
+
+            let player = document.getElementById("video");
+            player.src = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+            player.play();
             break;
 
         case "dash":
